868425c4f501cf564512ae457bea584dd61e5b16
Jared Wasinger <j-wasinger@hotmail.com>
2022-10-17 23:32:23 -0400

    gofmt.  fix ElementSize calculation

--- mont_context.go
+++ mont_context.go
@@ -1,9 +1,9 @@
 package mont_arith
 
 import (
+	"encoding/binary"
 	"errors"
 	"math/big"
-    "encoding/binary"
 )
 
 const limbSize = 8
@@ -68,47 +68,47 @@ func (m *Field) ModInv() *big.Int {
 }
 
 func uint64_array_to_le_bytes(val []uint64) []byte {
-	res := make([]byte, len(val) * 8)
+	res := make([]byte, len(val)*8)
 	for i := 0; i < len(val); i++ {
-        binary.LittleEndian.PutUint64(res[i*8:(i+1)*8], val[i])
+		binary.LittleEndian.PutUint64(res[i*8:(i+1)*8], val[i])
 	}
 
 	return res
 }
 
 func le_bytes_to_uint64_array(val []byte) []uint64 {
-    res := make([]uint64, len(val) / 8)
-	for i := 0; i < len(val) / 8; i++ {
-        res[i] = binary.LittleEndian.Uint64(val[i*8:(i+1)*8])
-    }
-    return res
+	res := make([]uint64, len(val)/8)
+	for i := 0; i < len(val)/8; i++ {
+		res[i] = binary.LittleEndian.Uint64(val[i*8 : (i+1)*8])
+	}
+	return res
 }
 
 // TODO this should not do allocation/copying.  should be just as fast as mulmont
 func (m *Field) ToMont(val []uint64) ([]uint64, error) {
-    // TODO ensure val is less than modulus
-    out_bytes := make([]byte, m.NumLimbs * 8)
+	// TODO ensure val is less than modulus
+	out_bytes := make([]byte, m.NumLimbs*8)
 	input_bytes := uint64_array_to_le_bytes(val)
 	r_squared_bytes := uint64_array_to_le_bytes(m.RSquared())
 
 	if err := m.MulMont(m, out_bytes, input_bytes, r_squared_bytes); err != nil {
 		return nil, err
 	}
-    return le_bytes_to_uint64_array(out_bytes), nil
+	return le_bytes_to_uint64_array(out_bytes), nil
 }
 
 func (m *Field) ToNorm(val []uint64) ([]uint64, error) {
-    // TODO ensure val is less than the modulus?
-    out_bytes := make([]byte, m.NumLimbs * 8)
+	// TODO ensure val is less than the modulus?
+	out_bytes := make([]byte, m.NumLimbs*8)
 	input_bytes := uint64_array_to_le_bytes(val)
-    one := make([]byte, len(val))
-    one[0] = 1
+	one := make([]byte, len(val))
+	one[0] = 1
 
 	if err := m.MulMont(m, out_bytes, input_bytes, one); err != nil {
 		return nil, err
 	}
 
-    return le_bytes_to_uint64_array(out_bytes), nil
+	return le_bytes_to_uint64_array(out_bytes), nil
 }
 
 func NewField(preset ArithPreset) *Field {
@@ -171,45 +171,45 @@ func (m *Field) SetMod(mod []uint64) error {
 
 	m.rSquared = IntToLimbs(rSquared, limbCount)
 
-		// want to compute r_val - (mod & (r_val - 1))
-		littleRVal, _ := new(big.Int).SetString("18446744073709551616", 10)
+	// want to compute r_val - (mod & (r_val - 1))
+	littleRVal, _ := new(big.Int).SetString("18446744073709551616", 10)
 
-		negModInt := new(big.Int)
-		negModInt.SetUint64(mod[0])
-		negModInt.Sub(littleRVal, negModInt)
-		modInv := new(big.Int)
-		modInv.ModInverse(negModInt, littleRVal)
+	negModInt := new(big.Int)
+	negModInt.SetUint64(mod[0])
+	negModInt.Sub(littleRVal, negModInt)
+	modInv := new(big.Int)
+	modInv.ModInverse(negModInt, littleRVal)
 
-		m.MontParamInterleaved = modInv.Uint64()
+	m.MontParamInterleaved = modInv.Uint64()
 
-		m.ModulusNonInterleaved = modInt
-		rVal := big.NewInt(1)
-		rVal.Lsh(rVal, 64*limbCount)
-		negModInt = new(big.Int)
-		negModInt.Neg(modInt)
-		m.MontParamNonInterleaved = new(big.Int)
-		m.MontParamNonInterleaved.ModInverse(negModInt, rVal)
-		m.mask = big.NewInt(1)
-		m.mask.Lsh(m.mask, 64*limbCount)
-		m.mask.Sub(m.mask, big.NewInt(1))
+	m.ModulusNonInterleaved = modInt
+	rVal := big.NewInt(1)
+	rVal.Lsh(rVal, 64*limbCount)
+	negModInt = new(big.Int)
+	negModInt.Neg(modInt)
+	m.MontParamNonInterleaved = new(big.Int)
+	m.MontParamNonInterleaved.ModInverse(negModInt, rVal)
+	m.mask = big.NewInt(1)
+	m.mask.Lsh(m.mask, 64*limbCount)
+	m.mask.Sub(m.mask, big.NewInt(1))
 
 	m.Modulus = make([]uint64, limbCount)
 	copy(m.Modulus, mod[:])
 	m.NumLimbs = limbCount
-	m.ElementSize = uint64(limbCount) * 4
+	m.ElementSize = uint64(limbCount) * 8
 
-    var genericMulMontCutoff uint = 64
-    if limbCount >= genericMulMontCutoff {
-        m.MulMont = MulMontNonInterleaved
-        m.AddMod = AddModGeneric
-        m.SubMod = SubModGeneric
-    } else {
-        m.MulMont = m.preset.MulMontImpls[limbCount-1]
+	var genericMulMontCutoff uint = 64
+	if limbCount >= genericMulMontCutoff {
+		m.MulMont = MulMontNonInterleaved
+		m.AddMod = AddModGeneric
+		m.SubMod = SubModGeneric
+	} else {
+		m.MulMont = m.preset.MulMontImpls[limbCount-1]
 
-        // TODO fix
-        m.AddMod = m.preset.AddModImpls[limbCount-1]
-        m.SubMod = m.preset.SubModImpls[limbCount-1]
-    }
+		// TODO fix (TODO ?? what was I thinking was wrong here?)
+		m.AddMod = m.preset.AddModImpls[limbCount-1]
+		m.SubMod = m.preset.SubModImpls[limbCount-1]
+	}
 
 	return nil
 }
