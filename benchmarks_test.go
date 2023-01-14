087bcd38cb46fad3217fe599c07cf8a2662a3616
Jared Wasinger <j-wasinger@hotmail.com>
2023-01-14 04:02:49 -0500

    memory benchmarks

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -2,7 +2,7 @@ package mont_arith
 
 import (
 	"fmt"
-	"math/big"
+	"math/rand"
 	"testing"
 )
 
@@ -14,71 +14,78 @@ func benchmarkMulMont(b *testing.B, limbCount uint, preset ArithPreset) {
 
 	err := montCtx.SetMod(mod)
 	if err != nil {
-		panic("error")
+		panic(err)
 	}
 
-	x := LimbsToInt(mod)
-	x = x.Sub(x, big.NewInt(100))
+	mem := make([]byte, limbCount*8*256)
 
-	y := new(big.Int).SetBytes(LimbsToInt(mod).Bytes())
-	y = y.Sub(y, big.NewInt(100))
-
-	outLimbs := make([]uint64, montCtx.NumLimbs)
-	xLimbs := IntToLimbs(x, limbCount)
-	yLimbs := IntToLimbs(y, limbCount)
-
-	outBytes := LimbsToLEBytes(outLimbs)
-	xBytes := LimbsToLEBytes(xLimbs)
-	yBytes := LimbsToLEBytes(yLimbs)
+	xIdxs := make([]int, 256)
+	yIdxs := make([]int, 256)
+	outIdxs := make([]int, 256)
+	for i := 0; i < 256; i++ {
+		outIdxs[i] = rand.Intn(255)
+		xIdxs[i] = rand.Intn(255)
+		yIdxs[i] = rand.Intn(255)
+	}
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		montCtx.MulMont(montCtx, outBytes, xBytes, yBytes)
+		montCtx.MulMont(montCtx, mem[outIdxs[i%256]:outIdxs[i%256]+48], mem[xIdxs[i%256]:xIdxs[i%256]+48], mem[yIdxs[i%256]:yIdxs[i%256]+48])
 	}
 }
 
 func benchmarkAddMod(b *testing.B, limbCount uint, preset ArithPreset) {
-	modLimbs := MaxModulus(limbCount)
-	mod := LimbsToInt(modLimbs)
+	mod := GenTestModulus(limbCount)
 	montCtx := NewField(preset)
 
-	// worst-case performance: unecessary final subtraction
-	err := montCtx.SetMod(modLimbs)
+	err := montCtx.SetMod(mod)
 	if err != nil {
-		panic("error")
+		panic(err)
 	}
-	x := new(big.Int).SetBytes(mod.Bytes())
-	x = x.Sub(x, big.NewInt(2))
-	y := big.NewInt(1)
-	outBytes := make([]byte, limbCount*8)
-	xBytes := LimbsToLEBytes(IntToLimbs(x, limbCount))
-	yBytes := LimbsToLEBytes(IntToLimbs(y, limbCount))
+
+	mem := make([]byte, limbCount*8*256)
+
+	xIdxs := make([]int, 256)
+	yIdxs := make([]int, 256)
+	outIdxs := make([]int, 256)
+	for i := 0; i < 256; i++ {
+		outIdxs[i] = rand.Intn(255)
+		xIdxs[i] = rand.Intn(255)
+		yIdxs[i] = rand.Intn(255)
+	}
 
 	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
-		montCtx.AddMod(montCtx, outBytes, xBytes, yBytes)
+		montCtx.AddMod(montCtx, mem[outIdxs[i%256]:outIdxs[i%256]+48], mem[xIdxs[i%256]:xIdxs[i%256]+48], mem[yIdxs[i%256]:yIdxs[i%256]+48])
 	}
 }
 
 func benchmarkSubMod(b *testing.B, limbCount uint, preset ArithPreset) {
-	modLimbs := MaxModulus(limbCount)
+	mod := GenTestModulus(limbCount)
 	montCtx := NewField(preset)
 
-	// worst-case performance: unecessary final subtraction
-	err := montCtx.SetMod(modLimbs)
+	err := montCtx.SetMod(mod)
 	if err != nil {
-		panic("error")
+		panic(err)
 	}
-	x := big.NewInt(1)
-	y := big.NewInt(0)
-	outBytes := make([]byte, limbCount*8)
-	xBytes := LimbsToLEBytes(IntToLimbs(x, limbCount))
-	yBytes := LimbsToLEBytes(IntToLimbs(y, limbCount))
+
+	mem := make([]byte, limbCount*8*256)
+
+	xIdxs := make([]int, 256)
+	yIdxs := make([]int, 256)
+	outIdxs := make([]int, 256)
+	for i := 0; i < 256; i++ {
+		outIdxs[i] = rand.Intn(255)
+		xIdxs[i] = rand.Intn(255)
+		yIdxs[i] = rand.Intn(255)
+	}
 
 	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
-		montCtx.SubMod(montCtx, outBytes, xBytes, yBytes)
+		montCtx.SubMod(montCtx, mem[outIdxs[i%256]:outIdxs[i%256]+48], mem[xIdxs[i%256]:xIdxs[i%256]+48], mem[yIdxs[i%256]:yIdxs[i%256]+48])
 	}
 }
 
